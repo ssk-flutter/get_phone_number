a5f5c92ec2256e016b28ec85b744482388807cfb
kimss <kimss@rsupport.com>
2020-12-19 16:35:47 +0900

    error exception on iOS

--- ios/Classes/SwiftGetPhoneNumberPlugin.swift
+++ ios/Classes/SwiftGetPhoneNumberPlugin.swift
@@ -3,12 +3,14 @@ import UIKit
 
 public class SwiftGetPhoneNumberPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "get_phone_number", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "ssk.d/get_phone_number", binaryMessenger: registrar.messenger())
     let instance = SwiftGetPhoneNumberPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
+    result(FlutterError(code: "UNAVAILABLE",
+                             message: "iOS does not support get phone number.",
+                             details: nil))
   }
 }
