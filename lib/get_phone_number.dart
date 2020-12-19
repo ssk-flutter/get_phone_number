9aaf5b8aa981f41a8f2ba95e82a61c2967e88130
kimss <kimss@rsupport.com>
2020-12-19 23:44:23 +0900

    Documentation

--- lib/get_phone_number.dart
+++ lib/get_phone_number.dart
@@ -2,10 +2,13 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+/// No dependency module
 class GetPhoneNumber {
   static const MethodChannel _channel =
       const MethodChannel('ssk.d/get_phone_number');
 
+  /// Get phone number with handle android permissions.
+  /// You don't have to handle exceptions.
   Future<String> getWithPermission() async {
     try {
       if (!(await hasPermission())) {
@@ -19,10 +22,14 @@ class GetPhoneNumber {
     }
   }
 
+  /// Check any permission granted to get the phone number.
   Future<bool> hasPermission() => _channel.invokeMethod('hasPermission');
 
+  /// Request permission to get the phone number.
   Future<bool> requestPermission() =>
       _channel.invokeMethod('requestPermission');
 
+  /// Get the phone number.
+  /// You may handle exceptions to avoid error message.
   Future<String> get() => _channel.invokeMethod('getPhoneNumber');
 }
